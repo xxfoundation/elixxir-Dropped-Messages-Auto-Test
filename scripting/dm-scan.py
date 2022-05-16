107ac57512e7adb80986cc66e79fcf365dce058c
Sydney Anne Erickson <sydney@elixxir.io>
2022-05-16 10:54:00 -0700

    scripting: save client logs to db, use token

--- scripting/dm-scan.py
+++ scripting/dm-scan.py
@@ -1,5 +1,6 @@
 import requests
 import dataset
+from tqdm.auto import trange, tqdm
 
 import timeutils
 
@@ -16,10 +17,10 @@ PROJECT_FULL_NAME="elixxir%2Fdropped-messages-auto-test"
 
 # https://git.xx.network/api/v4/projects/elixxir%2Fdropped-messages-auto-test/jobs/13950/artifacts/raw/delayedpickup/results/clients/parser.txt
 
-for PAGE in range(1, 200):
+for PAGE in trange(200, desc="Job page"):
     pipelines_r=requests.get("https://{}/api/v4/projects/{}/pipelines?per_page=100&page={}".format(GITLAB_URL, PROJECT_FULL_NAME, PAGE), headers={"PRIVATE-TOKEN":PRIVATE_TOKEN})
 
-    for pipeline in pipelines_r.json():
+    for pipeline in tqdm(pipelines_r.json(), desc="Jobs on page"):
         # Ignore cancelled jobs, these are human known to be broken
         # Also skip running, since results are not known yet
         if pipeline["status"] == "canceled" or pipeline["status"] == "running":
@@ -46,6 +47,16 @@ for PAGE in range(1, 200):
 
             parser_results = requests.get("https://{}/api/v4/projects/{}/jobs/{}/artifacts/delayedpickup/results/clients/parser.txt".format(GITLAB_URL, PROJECT_FULL_NAME, job["id"])).text
 
+            client42_wv_log = requests.get("https://{}/api/v4/projects/{}/jobs/{}/artifacts/delayedpickup/results/clients/client42-wv.log".format(GITLAB_URL, PROJECT_FULL_NAME, job["id"])).text
+            client42_wv_txt = requests.get("https://{}/api/v4/projects/{}/jobs/{}/artifacts/delayedpickup/results/clients/client42-wv.txt".format(GITLAB_URL, PROJECT_FULL_NAME, job["id"])).text
+            client42_log = requests.get("https://{}/api/v4/projects/{}/jobs/{}/artifacts/delayedpickup/results/clients/client42.log".format(GITLAB_URL, PROJECT_FULL_NAME, job["id"])).text
+            client42_txt = requests.get("https://{}/api/v4/projects/{}/jobs/{}/artifacts/delayedpickup/results/clients/client42.txt".format(GITLAB_URL, PROJECT_FULL_NAME, job["id"])).text
+
+            client43_wv_log = requests.get("https://{}/api/v4/projects/{}/jobs/{}/artifacts/delayedpickup/results/clients/client43-wv.log".format(GITLAB_URL, PROJECT_FULL_NAME, job["id"])).text
+            client43_wv_txt = requests.get("https://{}/api/v4/projects/{}/jobs/{}/artifacts/delayedpickup/results/clients/client43-wv.txt".format(GITLAB_URL, PROJECT_FULL_NAME, job["id"])).text
+            client43_log = requests.get("https://{}/api/v4/projects/{}/jobs/{}/artifacts/delayedpickup/results/clients/client43.log".format(GITLAB_URL, PROJECT_FULL_NAME, job["id"])).text
+            client43_txt = requests.get("https://{}/api/v4/projects/{}/jobs/{}/artifacts/delayedpickup/results/clients/client43.txt".format(GITLAB_URL, PROJECT_FULL_NAME, job["id"])).text
+
             jobs_table.insert(dict(
                 pipeline_id    = pipeline["id"],
                 status         = job["status"],
@@ -55,6 +66,14 @@ for PAGE in range(1, 200):
                 finished_time  = timeutils.str_timestamp_to_epoch(job["finished_at"]),
                 job_log        = job_log,
                 parser_results = parser_results,
+                client42_wv_log = client42_wv_log, 
+                client42_wv_txt = client42_wv_txt, 
+                client42_log = client42_log,
+                client42_txt = client42_txt,
+                client43_wv_log = client43_wv_log,
+                client43_wv_txt = client43_wv_txt,
+                client43_log = client43_log,
+                client43_txt = client43_txt
             ))
 
-            print("{}, {}, {}".format(job["status"], length_s, "https://git.xx.network/elixxir/dropped-messages-auto-test/-/jobs/{}".format(job["id"])))
+            tqdm.write("{}, {}, {}".format(job["status"], length_s, "https://git.xx.network/elixxir/dropped-messages-auto-test/-/jobs/{}".format(job["id"])))
