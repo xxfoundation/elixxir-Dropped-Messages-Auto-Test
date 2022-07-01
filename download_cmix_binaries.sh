75250f8ad3ac20895345e2d2f00a0e6b9bdd5757
Sydney Anne Erickson <sydney@elixxir.io>
2022-07-01 13:56:22 -0700

    go back to downloading release

--- download_cmix_binaries.sh
+++ download_cmix_binaries.sh
@@ -5,9 +5,9 @@ mkdir bin
 
 while [ ! -f "bin/client" ]; do
     if [ "$(uname)" == "Darwin" ]; then
-        curl -s -f -L -H "PRIVATE-TOKEN: $GITLAB_ACCESS_TOKEN" -o "bin/client" "https://git.xx.network/api/v4/projects/elixxir%2Fclient/jobs/artifacts/hotfix%2FIntegrationFixes/raw/release/client.darwin64?job=build"
+        curl -s -f -L -H "PRIVATE-TOKEN: $GITLAB_ACCESS_TOKEN" -o "bin/client" "https://git.xx.network/api/v4/projects/elixxir%2Fclient/jobs/artifacts/release/raw/release/client.darwin64?job=build"
     elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        curl -s -f -L -H "PRIVATE-TOKEN: $GITLAB_ACCESS_TOKEN" -o "bin/client" "https://git.xx.network/api/v4/projects/elixxir%2Fclient/jobs/artifacts/hotfix%2FIntegrationFixes/raw/release/client.linux64?job=build"
+        curl -s -f -L -H "PRIVATE-TOKEN: $GITLAB_ACCESS_TOKEN" -o "bin/client" "https://git.xx.network/api/v4/projects/elixxir%2Fclient/jobs/artifacts/release/raw/release/client.linux64?job=build"
     fi
     sleep 1
 done
